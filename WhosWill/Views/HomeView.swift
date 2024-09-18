0945931509422b130f563d33d5058d1de97cc615
WillLam-852 <willyin@connect.hku.hk>
2024-09-18 11:44:58 +0800

    ADD: App Icon + HeaderComponent

---
+++ WhosWill/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  WhosWill
+//
+//  Created by Lam Wun Yin on 18/9/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            HeaderComponent()
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}

6d80a1914fef9a196e5a0c13f9cdc6eb2d9a63b5
WillLam-852 <willyin@connect.hku.hk>
2024-09-18 11:10:48 +0800

    Initialize Project

---
+++ WhosWillTests/WhosWillTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhosWillTests.swift
+//  WhosWillTests
+//
+//  Created by Lam Wun Yin on 18/9/2024.
+//
+
+import Testing
+@testable import WhosWill
+
+struct WhosWillTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
